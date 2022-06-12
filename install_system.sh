40e9e8ecd11926733901b80516072051936fd886
Mohammad-Reza Azizi <mreza.azizi74@gmail.com>
2022-06-12 18:03:59 +0430

    Create install_system.sh

---
+++ install_system.sh
@@ -0,0 +1,51 @@
+# update system
+sudo apt update
+sudo apt upgrade
+
+# install pycharm-comminuty -- I used ubuntu software center
+sudo snap install pycharm-community --classic
+
+# install dbeaver
+sudo snap install dbeaver-ce
+
+# install cisco vpn
+sh anyconnect-linux64-4.10.04071-core-vpn-webdeploy-k9.sh
+
+# install vscode
+sudo snap install --classic code
+
+# install slack
+sudo snap install slack --classic
+
+# install firefox addons
+	# https://addons.mozilla.org/en-US/firefox/addon/adblock-plus/
+	# https://addons.mozilla.org/en-US/firefox/addon/setupvpn
+	
+# install neofetch
+sudo apt install neofetch
+
+# install git
+sudo apt install git
+
+# install python-realted things
+sudo apt install python3.10-venv
+sudo apt install python3-pip
+sudo apt install ipython3
+
+# install fish
+sudo apt-add-repository ppa:fish-shell/release-3 
+sudo apt update
+sudo apt install fish
+
+# install vlc player
+sudo snap install vlc
+
+# install google chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# install mariadb
+sudo apt install mariadb-server
+
+# install redis
+sudo apt install redis-server
