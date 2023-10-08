ab54a2e2e68055018aca45ebbc3da6f253ab98d7
Mohammad-Reza Azizi <mreza.azizi74@gmail.com>
2023-10-08 14:32:31 +0330

    added pycharm and datagrip aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
-# python virtualenvs
-alias tf="source ~/python_venvs/tf/bin/activate.fish"
-alias fl="source ~/python_venvs/fl/bin/activate.fish"
+function tpycharm
+  command sh ~/softwares/pycharm-2023.1.3/bin/pycharm.sh 1>/dev/null 2>&1 &
+end
+
+
+function tdatagrip
+  command sh ~/softwares/datagrip-2023.2/bin/datagrip.sh 1>/dev/null 2>&1 &
+end
